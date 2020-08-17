6ef01505c2a0e3877ebc8c61c50b770873fc51eb
tsightler <tsightler@gmail.com>
2020-08-16 20:06:33 -0400

    Update security-panel.js

--- devices/security-panel.js
+++ devices/security-panel.js
@@ -7,6 +7,7 @@ class SecurityPanel extends AlarmDevice {
         // Home Assistant component type and device class (set appropriate icon)
         this.component = 'alarm_control_panel'
         this.deviceData.mdl = 'Alarm Base Station'
+        this.deviceData.name = this.device.location.name + ' Alarm'
 
         // Build required MQTT topics for device
         this.deviceTopic = this.alarmTopic+'/'+this.component+'/'+this.deviceId
