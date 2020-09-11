ba2d1318d9e4da12451fe3a131d8a5f8ac75bf4e
tsightler <tsightler@gmail.com>
2020-09-11 00:44:07 -0400

    Fix debug output

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -12,8 +12,15 @@ if [ -f /data/options.json ]; then
         exec /init/run-prod.sh
     fi 
 else
-    if [ ! -z "$DEBUG" ]; then
-        echo Running ring-mqtt.js version $(cat /ring-mqtt/package.json | grep version | cut -f4 -d'"')...
+    if [ -n ${DEBUG+x} ]; then
+        echo "-------------------------------------------------------"
+        echo "| Ring Device Integration via MQTT                    |"
+        echo "|                                                     |"
+        echo "| Report issues at:                                   |"
+        echo "| https://github.com/tsightler/ring-mqtt              |"
+        echo "-------------------------------------------------------"
+        echo Running ring-mqtt.js version $(cat /ring-mqtt/package.json | grep version | cut -f4 -d'"')
+        echo "-------------------------------------------------------"
     fi
     # If there's no options.json assume we are in standard Docker container
     ISDOCKER=true exec /ring-mqtt/ring-mqtt.js
