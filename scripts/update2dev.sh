9a1d49806794430f92176e687e85d804f45e04ac
tsightler <tsightler@gmail.com>
2020-09-11 10:40:53 -0400

    Make scripts executable

--- scripts/update2dev.sh
+++ scripts/update2dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 echo "-------------------------------------------------------"
-echo "Updating ring-mqtt to latest dev branch version..."
+echo "Updating ring-mqtt to Github dev branch..."
 cd /app
 mv ring-mqtt ring-mqtt.orig
 git clone -b dev https://github.com/tsightler/ring-mqtt
 cd ring-mqtt
 npm install
-chmod +x ring-mqtt.js
+chmod +x ring-mqtt.js scripts/*.sh
 echo "-------------------------------------------------------"
--- scripts/update2latest.sh
+++ scripts/update2latest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 echo "-------------------------------------------------------"
-echo "Updating ring-mqtt to latest main branch version..."
+echo "Updating ring-mqtt to Github main branch..."
 cd /app
 mv ring-mqtt ring-mqtt.orig
 git clone https://github.com/tsightler/ring-mqtt
 cd ring-mqtt
 npm install
-chmod +x ring-mqtt.js
+chmod +x ring-mqtt.js scripts/*.sh
 echo "-------------------------------------------------------"
