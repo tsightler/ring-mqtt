c0eb497f17372b0cf4374588f59a212b20fc45b6
tsightler <tsightler@gmail.com>
2020-09-14 20:38:42 -0400

    Fix running branches unprivileged

--- scripts/update2dev.sh
+++ scripts/update2dev.sh
@@ -3,8 +3,8 @@ HOME=/app
 echo "Updating ring-mqtt to the development version..."
 cd /app
 rm ring-mqtt
-git clone -b dev https://github.com/tsightler/ring-mqtt-dev
-ln -s /app/ring-mqtt-dev /app/ring-mqtt
+git clone -b dev https://github.com/tsightler/ring-mqtt ring-mqtt-dev
+ln -s ring-mqtt-dev ring-mqtt
 cd /app/ring-mqtt
 echo "Installing node module dependencies, please wait..."
 npm install --no-progress > /dev/null 2>&1
