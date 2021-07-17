e626c01a3a6af780d403cee56cb1f3939f4fd41f
tsightler <tsightler@gmail.com>
2021-07-16 20:52:38 -0400

    Update keypad.js

--- devices/keypad.js
+++ devices/keypad.js
@@ -8,33 +8,29 @@ class Keypad extends AlarmDevice {
         // Device data for Home Assistant device registry
         this.deviceData.mdl = 'Security Keypad'
 
-        if (this.config.enable_volume) {
-            // Build required MQTT topics for volume control
-            this.stateTopic_volume = this.deviceTopic+'/audio/volume_state'
-            this.commandTopic_volume = this.deviceTopic+'/audio/volume_command'
-            this.configTopic_volume = 'homeassistant/number/'+this.locationId+'/'+this.deviceId+'_volume/config'
-        }
+        // Build required MQTT topics for volume control
+        this.stateTopic_volume = this.deviceTopic+'/audio/volume_state'
+        this.commandTopic_volume = this.deviceTopic+'/audio/volume_command'
+        this.configTopic_volume = 'homeassistant/number/'+this.locationId+'/'+this.deviceId+'_volume/config'
     }
         
     initDiscoveryData() {
         // Build the MQTT discovery messages if volume control is enabled
-        if (this.stateTopic_volume) {
-            this.discoveryData.push({
-                message: {
-                    name: this.device.name+' Volume',
-                    unique_id: this.deviceId+'_volume',
-                    availability_topic: this.availabilityTopic,
-                    payload_available: 'online',
-                    payload_not_available: 'offline',
-                    state_topic: this.stateTopic_volume,
-                    command_topic: this.commandTopic_volume,
-                    min: 0,
-                    max: 100,
-                    device: this.deviceData
-                },
-                configTopic: this.configTopic_audio
-            })
-        }
+        this.discoveryData.push({
+            message: {
+                name: this.device.name+' Volume',
+                unique_id: this.deviceId+'_volume',
+                availability_topic: this.availabilityTopic,
+                payload_available: 'online',
+                payload_not_available: 'offline',
+                state_topic: this.stateTopic_volume,
+                command_topic: this.commandTopic_volume,
+                min: 0,
+                max: 100,
+                device: this.deviceData
+            },
+            configTopic: this.configTopic_audio
+        })
         
         // Device has no sensors, only publish info data
         this.initInfoDiscoveryData()
