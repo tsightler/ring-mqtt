b99d6e2871db27978714768ec8a69ceaefb2c2bb
tsightler <tsightler@gmail.com>
2021-07-17 20:23:48 -0400

    Update fan.js

--- devices/fan.js
+++ devices/fan.js
@@ -40,7 +40,7 @@ class Fan extends AlarmDevice {
                 preset_mode_state_topic: this.stateTopic_preset,
                 preset_mode_command_topic: this.commandTopic_preset,
                 preset_modes: [ "low", "medium", "high" ],
-                speed_range_min: 10,
+                speed_range_min: 11,
                 speed_range_max: 100,
                 device: this.deviceData
             },
@@ -112,21 +112,28 @@ class Fan extends AlarmDevice {
     async setFanPercent(message) {
         if (isNaN(message)) {
             debug('Fan speed percent command received but value is not a number')
-        } else if (!(message >= 10 && message <= 100)) {
-            debug('Fan speed percent command received but out of range (10-100)')
-        } else {
-            this.targetFanPercent = message
-            debug('Seting fan speed percentage to '+this.targetFanPercent+'% for fan Id: '+this.deviceId)
-            debug('Location Id: '+ this.locationId)
-            this.device.setInfo({ device: { v1: { level: this.targetFanPercent / 100 } } })
-
-            // Automatically turn on fan when level is sent.
-            // Home assistant normally does this but we want the
-            // same behavior for non-HA users as well.
-            await utils.sleep(1)
-            const fanState = this.device.data.on ? "ON" : "OFF"
-            if (fanState == 'OFF') { this.setFanState('on') }
+            return
         }
+
+        this.targetFanPercent = message
+        if (this.targetFanPercent < 10) {
+            debug('Received fan speed percent value of '+this.targetFanPercent+'% is outside of valid range (10-100%)')
+            this.targetFanPercent = 10 
+        } else if (this.targetFanPercent > 100) {
+            debug('Received fan speed percent value of '+this.targetFanPercent+'% is outside of valid range (10-100%)')
+            this.targetFanPercent = 100
+        }
+
+        debug('Seting fan speed percentage to '+this.targetFanPercent+'% for fan: '+this.deviceId)
+        debug('Location Id: '+ this.locationId)
+        this.device.setInfo({ device: { v1: { level: this.targetFanPercent / 100 } } })
+
+        // Automatically turn on fan when level is sent.
+        // Home assistant normally does this but we want the
+        // same behavior for non-HA users as well.
+        await utils.sleep(1)
+        const fanState = this.device.data.on ? "ON" : "OFF"
+        if (fanState == 'OFF') { this.setFanState('on') }
     }
 
     // Set fan speed state from received MQTT command message
