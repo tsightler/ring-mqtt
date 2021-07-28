7117ce222778f73141acc006d80c974d993da971
tsightler <tsightler@gmail.com>
2021-07-28 10:31:00 -0400

    Update ring-mqtt.js

--- ring-mqtt.js
+++ ring-mqtt.js
@@ -168,12 +168,12 @@ async function updateRingData(mqttClient, ringClient) {
             const deviceId = (device instanceof RingCamera) ? device.data.device_id : device.id
             const foundDevice = ringDevices.find(d => d.deviceId == deviceId && d.locationId == location.locationId)
             if (foundDevice) {
-                debug(colors.green('  Existing device '+foundDevice.deviceData.name+' of type: '+device.deviceType+', Id: '+deviceId))
+                debug(colors.green('  Existing device: '+foundDevice.deviceData.name+' ('+device.deviceType+', '+deviceId+')'))
             } else {
                 const newDevice = getDevice(device, mqttClient)
                 if (newDevice) {
                     ringDevices.push(newDevice)
-                    debug(colors.green('  New device of type: '+newDevice.deviceData.name+', Id: '+deviceId))
+                    debug(colors.green('  New device: '+newDevice.deviceData.name+' ('+device.deviceType+', '+deviceId+')'))
                 } else {
                     // Save unsupported device type
                     unsupportedDevices.push(device.deviceType)
