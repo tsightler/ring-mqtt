2db8f1a56b273a610903c3d0f55af518021104af
tsightler <tsightler@gmail.com>
2021-08-02 15:08:55 -0400

    Update chime.js

--- devices/chime.js
+++ devices/chime.js
@@ -117,6 +117,8 @@ class Chime {
     }
 
     publishData() {
+        debug(this.topic)
+        debug(this.device)
         const volumeState = this.device.data.settings.volume
         const snoozeState = Boolean(this.device.data.do_not_disturb.seconds_left) ? 'ON' : 'OFF'
         // Publish sensor state
