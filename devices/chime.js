7d9b3d3e040360daf642e7d42189c3563e3c3710
tsightler <tsightler@gmail.com>
2021-08-05 22:44:41 -0400

    Update chime.js

--- devices/chime.js
+++ devices/chime.js
@@ -53,7 +53,7 @@ class Chime extends RingDevice {
             info: {
                 type: 'sensor',
                 valueTemplate: '{{ value_json["lastUpdate"] | default }}',
-                unitOfMeasurement: 'timestamp'
+                deviceClass: 'timestamp'
             }
         }
     }
