37c82eba31ab24cc5af85365321e8fe476366fcb
tsightler <tsightler@gmail.com>
2021-08-05 20:34:31 -0400

    Update chime.js

--- devices/chime.js
+++ devices/chime.js
@@ -45,7 +45,7 @@ class Chime extends RingDevice {
             },
             info: {
                 type: 'sensor',
-                valueTemplate: '{{value_json["wirelessSignal"]}}',
+                valueTemplate: '{{ value_json["wirelessSignal"] | default }}',
                 unitOfMeasurement: 'RSSI'
             }
         }
