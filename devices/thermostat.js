8a61161948cb666282d371937b39b355232c1eeb
tsightler <tsightler@gmail.com>
2021-08-10 12:00:02 -0400

    Update thermostat.js

--- devices/thermostat.js
+++ devices/thermostat.js
@@ -5,6 +5,7 @@ class Thermostat extends RingSocketDevice {
     constructor(deviceInfo) {
         super(deviceInfo)
         this.deviceData.mdl = 'Thermostat'
+        this.foundComponentDevices = false
 
         /* this.entities.motion = {
             component: 'binary_sensor',
@@ -14,31 +15,59 @@ class Thermostat extends RingSocketDevice {
         this.initAttributeEntities()
     }
 
-    publishData() {
-        if (!this.subscribed) {
-            // First publish so we need to find the temperature sensor as well
-            this.findTemperatureSensor()
-        }
-        debug(JSON.stringify(this.device.data))
-        this.publishAttributes()
-    }
-
-    async findTemperatureSensor() {
+    async findComponentDevices() {
+        this.foundComponentDevices = true
         const allDevices = await this.device.location.getDevices()
+
         this.temperatureSensor = allDevices.find(device => device.data.parentZid === this.deviceId && device.deviceType === 'sensor.temperature')
         if (this.temperatureSensor) {
             debug (`Found temperature sensor ${this.temperatureSensor.id} for thermostat ${this.deviceId}`)
             // First publish also subscribe to temperature sensor updates
-            this.temperatureSensor.onData.subscribe(data => { 
-                this.publishTemeratureData(data)
+            this.temperatureSensor.onData.subscribe(() => { 
+                this.publishData()
             })
         } else {
             debug (`Could not find temerature sensor for thermostat ${this.deviceId}`)
-        } 
+        }
+
+        this.operatingStatus = allDevices.find(device => device.data.parentZid === this.deviceId && device.deviceType === 'thermostat-operating-status')
+        if (this.operatingStatus) {
+            debug (`Found operating status sensor ${this.operatingStatus.id} for thermostat ${this.deviceId}`)
+            // First publish also subscribe to temperature sensor updates
+            this.operatingStatus.onData.subscribe(() => { 
+                this.publishData()
+            })
+        } else {
+            debug (`Could not find operating status sensor for thermostat ${this.deviceId}`)
+        }
     }
 
-    publishTemeratureData(data) {
-        debug(JSON.stringify(data))
+    async publishData() {
+        if (!this.foundComponentDevices) {
+            // First publish so we need to find the other thermostat components as well
+            await this.findComponentDevices()
+        }
+        debug(`fanMode: ${this.device.data.fanMode}`)
+        debug(`mode: ${this.device.data.mode}`)
+        debug(this.device.data.modeSetpoints)
+        debug(`setPoint: ${this.device.data.setPoint}`)
+        debug(`setPointMin: ${this.device.data.setPointMin}`)
+        debug(`setPointMax: ${this.device.data.setPointMax}`)
+
+        if (this.operatingStatus) {
+            debug(`isCoolOn: ${this.operatingStatus.data.isCoolOn}`)
+            debug(`isHeatOn: ${this.operatingStatus.data.isHeatOn}`)
+            debug(`isCool2ndOn: ${this.operatingStatus.data.isCool2ndOn}`)
+            debug(`isHeat2ndOn: ${this.operatingStatus.data.isHeat2ndOn}`)
+            debug(`operatingMode: ${this.operatingStatus.data.operatingMode}`)
+        }
+        if (this.temperatureSensor) {
+            debug(`Temperature: ${this.temperatureSensor.data.celsius}`)
+            debug(`faultHigh: ${this.temperatureSensor.data.faultHigh}`)
+            debug(`faultLow: ${this.temperatureSensor.data.faultLow}`)
+            debug(`faulted: ${this.temperatureSensor.data.faulted}`)
+        }
+        this.publishAttributes()
     }
 }
 
