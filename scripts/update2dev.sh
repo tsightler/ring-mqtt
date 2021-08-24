a0137b10b6badb2637a201fcfc14062363afd5cd
tsightler <tsightler@gmail.com>
2021-08-23 22:33:43 -0400

    Add rtsp-simple-server
    * Temporary add to dev update script to  download/install rtsp-simple-server

--- scripts/update2dev.sh
+++ scripts/update2dev.sh
@@ -11,3 +11,23 @@ echo "Installing node module dependencies, please wait..."
 npm install --no-progress > /dev/null 2>&1
 chmod +x ring-mqtt.js scripts/*.sh
 echo "-------------------------------------------------------"
+echo "Downloading and installing rtsp-simple-server"
+apk add --no-cache mosquitto-clients
+APKARCH="$(dpkg --print-architecture)"
+case "${APKARCH}" in
+    'x86_64')
+        wget -O rtsp-simple-server_binary.tar.gz https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.2/rtsp-simple-server_v0.17.2_linux_amd64.tar.gz
+        ;;
+    'aarch64')
+        wget -O rtsp-simple-server_binary.tar.gz https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.2/rtsp-simple-server_v0.17.2_linux_arm64v8.tar.gz
+        ;;
+    'armv7')
+        wget -O rtsp-simple-server_binary.tar.gz https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.2/rtsp-simple-server_v0.17.2_linux_armv7.tar.gz
+        ;;
+    'armhf')
+        wget -O rtsp-simple-server_binary.tar.gz https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.2/rtsp-simple-server_v0.17.2_linux_armv6.tar.gz
+        ;;
+    *) echo >&2 "error: unsupported architecture '$dpkgArch' (likely packaging update needed)"; exit 1 ;;
+esac
+tar zxvfp rtsp-simple-server_binary.tar.gz
+echo "-------------------------------------------------------"
