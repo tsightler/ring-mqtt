ae729ebd403c05fb4527d790e999543852987202
tsightler <tsightler@gmail.com>
2021-08-24 12:45:45 -0400

    Update camera.js

--- devices/camera.js
+++ devices/camera.js
@@ -180,8 +180,7 @@ class Camera extends RingPolledDevice {
     addRtspPath() {
         const rtspPathConfig = JSON.stringify({
             source: 'publisher',
-            //runOnDemand: 'mosquitto_pub -u ${MQTTUSER} -P ${MQTTPASSWORD} -h ${MQTTHOST} -p ${MQTTPORT} -t '+this.deviceTopic+'/stream/command -m ON',
-            runOnDemand: 'mosquitto_pub --version',
+            runOnDemand: 'mosquitto_pub -u ${MQTTUSER} -P ${MQTTPASSWORD} -h ${MQTTHOST} -p ${MQTTPORT} -t '+this.deviceTopic+'/stream/command -m ON',
             runOnDemandRestart: true,
             runOnDemandStartTimeout: 20,
             runOnDemandCloseAfter: 5
