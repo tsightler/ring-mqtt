33f06911e7ed0324df00420c69eeefaf84903ba0
tsightler <tsightler@gmail.com>
2021-08-23 22:46:20 -0400

    Dev downloads rtsp-simple-server

--- scripts/run-addon.sh
+++ scripts/run-addon.sh
@@ -6,7 +6,7 @@ elif [ "${BRANCH}" = "dev" ]; then
     echo "-------------------------------------------------------"
     echo "Downloading and installing rtsp-simple-server"
     apk add --no-cache mosquitto-clients
-    APKARCH="$(dpkg --print-architecture)"
+    APKARCH="$(apk --print-arch)"
     case "${APKARCH}" in
         'x86_64')
             wget -O rtsp-simple-server_binary.tar.gz https://github.com/aler9/rtsp-simple-server/releases/download/v0.17.2/rtsp-simple-server_v0.17.2_linux_amd64.tar.gz
