446d27c17e112dbdcafd81b360698181f84a8b84
tsightler <tsightler@gmail.com>
2021-08-28 14:32:37 -0400

    Update start-stream.sh

--- scripts/start-stream.sh
+++ scripts/start-stream.sh
@@ -8,15 +8,15 @@ mosquitto_pub -i "${CLIENT_NAME}" -u "${MQTTUSER}" -P "${MQTTPASSWORD}" -h "${MQ
 
 mosquitto_sub -i "${CLIENT_NAME}" -u "${MQTTUSER}" -P "${MQTTPASSWORD}" -h "${MQTTHOST}" -p "${MQTTPORT}" -t "${STATE_TOPIC}" | while read -r message
 do
-   echo "This is a great message: ${message}"
+   echo "                         This is a great message: ${message}"
    if [ "${message}" = "OFF" ]; then
-       echo "We really should exit, killing everything...."
+       echo "                         We really should exit, killing everything...."
        mosquitto_pid=`ps -ef | grep mosquitto_sub | grep "${CLIENT_NAME}" | tr -s ' ' | cut -d ' ' -f2`
-       echo "${mosquitto_pid}"
+       echo "                         ${mosquitto_pid}"
        kill ${mosquitto_pid}
        exit
    fi
 done
 
-echo "We're outta here! Exiting the stream script..."
+echo "                         We're outta here! Exiting the stream script..."
 exit
