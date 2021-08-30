e7b5d9a666908aff2a92b7e5755457e39adbb4dd
tsightler <tsightler@gmail.com>
2021-08-30 02:03:56 -0400

    Better error messages

--- scripts/start-stream.sh
+++ scripts/start-stream.sh
@@ -12,7 +12,7 @@ reset='\033[0m'
 
 ctrl_c() {
     if [ -z ${reason} ]; then 
-        echo -e "Deactivating live stream for camera ${green}${client_name}${reset} due to signal from RTSP server (likely no active streams)"
+        echo -e "${green}[${client_name}]${reset} Deactivating live stream due to signal from RTSP server (likely no more active streams)"
         mosquitto_pub -i "${client_id}" -u "${MQTTUSER}" -P "${MQTTPASSWORD}" -h "${MQTTHOST}" -p "${MQTTPORT}" -t "${command_topic}" -m "OFF"
     fi
     # Cheesy, but there should only ever be one process per client active at any time so this works for now
@@ -23,7 +23,7 @@ ctrl_c() {
 
 trap ctrl_c INT TERM QUIT
 
-echo -e "Activating live stream for camera ${green}${client_name%:}${reset} via topic ${blue}${command_topic}${reset}"
+echo -e "${green}[${client_name}]${reset} Activating live stream via topic ${blue}${command_topic}${reset}"
 mosquitto_pub -i "${client_id}" -u "${MQTTUSER}" -P "${MQTTPASSWORD}" -h "${MQTTHOST}" -p "${MQTTPORT}" -t "${command_topic}" -m "ON"
 
 while read -u 10 message
@@ -31,23 +31,23 @@ do
     stream_state=`echo ${message} | jq -r '.streamState'`
     case ${stream_state,,} in
         activating)
-            echo -e "Live stream for camera ${green}${client_name}${reset} has entered activating state"
+            echo -e "${green}[${client_name}]${reset} Camera live stream has succesfully entered activating state"
             ;;
         active)
-            echo -e "Live stream for camera ${green}${client_name}${reset} has been successfully activated"
+            echo -e "${green}[${client_name}]${reset} Camera live stream has been successfully activated"
             ;;
         inactive)
-            echo -e "${yellow}Live stream for camera ${green}${client_name}${yellow} has gone inactive, exiting...${reset}"
+            echo -e "${green}[${client_name}]${yellow} Camera live stream has gone inactive, exiting...${reset}"
             reason='inactive'
             ctrl_c
             ;;
         failed)
-            echo -e "${red}ERROR - Live stream for camera ${green}${client_name%:}${red} failed to activate, exiting...${reset}"
+            echo -e "${green}[${client_name}]${red} ERROR - Camera live stream failed to activate, exiting...${reset}"
             reason='failed'
             ctrl_c
             ;;
         *)
-            echo -e "${red}ERROR - Received unknown stream state for camera ${green}${client_name%:}${red} on topic ${blue}${state_topic}${reset}"
+            echo -e "${green}[${client_name}]${red} ERROR - Unknown live stream state received on topic ${blue}${state_topic}${reset}"
             ;;
     esac
 done 10< <( mosquitto_sub -q 1 -i "${client_id}" -u "${MQTTUSER}" -P "${MQTTPASSWORD}" -h "${MQTTHOST}" -p "${MQTTPORT}" -t "${state_topic}" )
