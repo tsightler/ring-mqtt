dac3746598788723f6b1e2a87ecd4aab1e62609c
tsightler <tsightler@gmail.com>
2021-08-30 22:42:29 -0400

    Update update2dev.sh

--- scripts/update2dev.sh
+++ scripts/update2dev.sh
@@ -9,54 +9,54 @@ if [ ! -d /app/ring-mqtt-dev ]; then
     npm install --no-progress > /dev/null 2>&1
     chmod +x ring-mqtt.js scripts/*.sh
     exec /app/ring-mqtt-dev/scripts/update2dev.sh
-fi
 
-cd /app/ring-mqtt-dev
-echo "Adding mostquitto-clients..."
-apk add --no-cache mosquitto-clients
-echo "Downloading and installing rtsp-simple-server..."
-APKARCH="$(apk --print-arch)"
-mkdir -p bin; cd bin
-case "${APKARCH}" in
-    'x86_64')
-        curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_amd64.tar.gz | tar zxvf - rtsp-simple-server
-        ;;
-    'aarch64')
-        curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_arm64v8.tar.gz | tar zxvf - rtsp-simple-server
-        ;;
-    'armv7')
-        curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_armv7.tar.gz | tar zxvf - rtsp-simple-server
-        ;;
-    'armhf')
-        curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_armv6.tar.gz | tar zxvf - rtsp-simple-server
-        ;;
-    *) 
-        echo >&2 "ERROR: Unsupported architecture '$APKARCH'"; 
-        exit 1 
-        ;;
-esac
-cd ..
-echo "-------------------------------------------------------"
-echo "Downloading and installing s6-overlay..."
+    cd /app/ring-mqtt-dev
+    echo "Adding mostquitto-clients..."
+    apk add --no-cache mosquitto-clients
+    echo "Downloading and installing rtsp-simple-server..."
+    APKARCH="$(apk --print-arch)"
+    mkdir -p bin; cd bin
     case "${APKARCH}" in
-    'x86_64')
-        curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-amd64.tar.gz" | tar zxvf - -C /
-        ;;
-    'aarch64')
-        curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-aarch64.tar.gz" | tar zxvf - -C /
-        ;;
-    'armv7')
-        curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-arm.tar.gz" | tar zxvf - -C /
-        ;;
-    'armhf')
-        curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-armhf.tar.gz" | tar zxvf - -C /
-        ;;
-    *) 
-        echo >&2 "ERROR: Unsupported architecture '$APKARCH'"; 
-        exit 1 
-        ;;
-esac
-mkdir -p /etc/fix-attrs.d
-mkdir -p /etc/services.d
-cp -a /app/ring-mqtt-dev/s6-etc/* /
-echo "-------------------------------------------------------"
+        'x86_64')
+            curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_amd64.tar.gz | tar zxf - rtsp-simple-server
+            ;;
+        'aarch64')
+            curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_arm64v8.tar.gz | tar zxf - rtsp-simple-server
+            ;;
+        'armv7')
+            curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_armv7.tar.gz | tar zxf - rtsp-simple-server
+            ;;
+        'armhf')
+            curl -L -s https://raw.githubusercontent.com/tsightler/rtsp-simple-server/main/release-custom/rtsp-simple-server_v0.17.2-21-g43b10dc_linux_armv6.tar.gz | tar zxf - rtsp-simple-server
+            ;;
+        *) 
+            echo >&2 "ERROR: Unsupported architecture '$APKARCH'"; 
+            exit 1 
+            ;;
+    esac
+    cd ..
+    echo "-------------------------------------------------------"
+    echo "Downloading and installing s6-overlay..."
+        case "${APKARCH}" in
+        'x86_64')
+            curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-amd64.tar.gz" | tar zxf - -C /
+            ;;
+        'aarch64')
+            curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-aarch64.tar.gz" | tar zxf - -C /
+            ;;
+        'armv7')
+            curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-arm.tar.gz" | tar zxf - -C /
+            ;;
+        'armhf')
+            curl -L -s "https://github.com/just-containers/s6-overlay/releases/download/v2.2.0.3/s6-overlay-armhf.tar.gz" | tar zxf - -C /
+            ;;
+        *) 
+            echo >&2 "ERROR: Unsupported architecture '$APKARCH'"; 
+            exit 1 
+            ;;
+    esac
+    mkdir -p /etc/fix-attrs.d
+    mkdir -p /etc/services.d
+    cp -a /app/ring-mqtt-dev/s6-etc/* /
+    echo "-------------------------------------------------------"
+fi
