bda7ddf6a9b09cfc09a8ac5331bd2f5192a20c45
tsightler <tsightler@gmail.com>
2021-09-17 01:55:19 -0400

    Release 4.8.3

--- devices/camera.js
+++ devices/camera.js
@@ -134,7 +134,8 @@ class Camera extends RingPolledDevice {
                         : []),
                     'Motion 1', 'Motion 2', 'Motion 3', 'Motion 4', 'Motion 5',
                     'On-demand 1', 'On-demand 2', 'On-demand 3', 'On-demand 4', 'On-demand 5'
-                ]
+                ],
+                attributes: true
             },
             ...this.device.isDoorbot ? {
                 ding: {
