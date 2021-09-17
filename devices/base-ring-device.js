5a20a4ec3eeee9f5660c85c5e5b13dab89e657b2
tsightler <tsightler@gmail.com>
2021-09-17 14:57:00 -0400

    Release 4.8.3

--- devices/base-ring-device.js
+++ devices/base-ring-device.js
@@ -181,7 +181,7 @@ class RingDevice {
 
     // Publish state messages with debug
     publishMqtt(topic, message, isDebug) {
-        if (isDebug) { debug(colors.green(`[${this.deviceData.name}] `)+colors.blue(`${topic} `)+colors.cyan(`${message}`)) }
+        if (isDebug) { this.debug(colors.blue(`${topic} `)+colors.cyan(`${message}`)) }
         this.mqttClient.publish(topic, message, { qos: 1 })
     }
 
