d47b4c5382f9bc24da39cb0309fb2bd7a95d83c7
tsightler <tsightler@gmail.com>
2022-02-06 19:59:29 -0500

    Modularize config functions

--- ring-mqtt.js
+++ ring-mqtt.js
@@ -444,6 +444,7 @@ const main = async(generatedToken) => {
     let stateFile = '/data/ring-state.json'
     let ringClient
     let mqttClient
+    debug(config.data)
 
     if (config.runMode === 'addon') {
         // For addon config is performed via Web UI
