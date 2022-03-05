f5d4ab5a34b5d0aeff9ce3bdacac2365534cabc6
tsightler <tsightler@gmail.com>
2022-03-04 21:02:03 -0500

    Fix token app

--- ring-mqtt.js
+++ ring-mqtt.js
@@ -68,26 +68,16 @@ const main = async(generatedToken) => {
             await utils.sleep(10)
         }
 
-        if (await ring.init(state.data, generatedToken)) {
-            // Subscribe to token update events and save new tokens to state file
-            ring.client.onRefreshTokenUpdated.subscribe(({ newRefreshToken, oldRefreshToken }) => {
-                state.updateToken(newRefreshToken, oldRefreshToken)
-            })
-
-            // Only leave the web UI active if this is the addon
-            if (process.env.RUNMODE === 'addon') {
-                tokenApp.stop()
-            }
-        } else {
-            debug(colors.brightRed('Failed to connect to Ring API using saved token, generate a new token using the Web UI.'))
-            debug(colors.brightRed('Authentication will be automatically retried in 60 seconds using the existing token.'))
+        if (!await ring.init(state, generatedToken)) {
+            debug(colors.brightRed('Failed to connect to Ring API using saved token, generate a new token using the Web UI'))
+            debug(colors.brightRed('or wait 60 seconds to automatically retry authentication using the existing token'))
             tokenApp.start()
             await utils.sleep(60)
             main()
         }
     } else {
         // If a refresh token was not found, start Web UI for token generator
-        debug(colors.brightRed('No refresh token was found in state file, generate a token using the Web UI.'))
+        debug(colors.brightRed('No refresh token was found in state file, generate a token using the Web UI'))
         tokenApp.start()
     }
 }
