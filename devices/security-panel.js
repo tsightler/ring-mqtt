7a9aa1adab10a4f7a33bb2456e66670fc7dd9227
tsightler <tsightler@gmail.com>
2022-05-09 21:14:19 -0400

    Release v5.0.0

--- devices/security-panel.js
+++ devices/security-panel.js
@@ -35,7 +35,13 @@ class SecurityPanel extends RingSocketDevice {
         }
     }
 
-    publishState() {
+    publishState(data) {
+        const isPublish = data === undefined ? true : false
+        if (isPublish) {
+            // Eventually remove this but for now this attempts to delete the old light component based volume control from Home Assistant
+            this.mqttPublish('homeassistant/switch/'+this.locationId+'/'+this.deviceId+'_bypass/config', '', false)
+        }
+
         let alarmMode
         const alarmInfo = this.device.data.alarmInfo ? this.device.data.alarmInfo : []
 
