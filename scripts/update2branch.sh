817476c108c3a32d43dd762094c3fb83d5a8caa4
tsightler <tsightler@gmail.com>
2022-08-04 17:38:44 -0400

    Use rtsp-simple-server v0.19.3 for dev branch

--- scripts/update2branch.sh
+++ scripts/update2branch.sh
@@ -20,4 +20,18 @@ if [ ! -d "/app/ring-mqtt-${BRANCH}" ]; then
 else
     # Branch has already been initialized, run any post-update command here
     echo "The ring-mqtt-${BRANCH} has been updated."
+    rm -f /usr/local/bin/rtsp-simple-server
+    APKARCH="$(apk --print-arch)"
+    case "${APKARCH}" in
+        x86_64)
+            RSSARCH="amd64";;
+        aarch64)
+            RSSARCH="arm64v8";;
+        armv7|armhf)
+            RSSARCH="armv7";;
+        *)
+            echo >&2 "ERROR: Unsupported architecture '$APKARCH'"
+            exit 1;;
+    esac
+    curl -L -s "https://github.com/aler9/rtsp-simple-server/releases/download/v0.19.3/rtsp-simple-server_v0.19.3_linux_${RSSARCH}.tar.gz" | tar zxf - -C /usr/local/bin rtsp-simple-server
 fi
