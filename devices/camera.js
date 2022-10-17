6194547a7e79f56e1ffc7a04382d888640e48f88
tsightler <tsightler@gmail.com>
2022-10-17 09:45:32 -0400

    Update camera.js

--- devices/camera.js
+++ devices/camera.js
@@ -673,7 +673,7 @@ class Camera extends RingPolledDevice {
         try {
             this.data.stream.event.session = spawn(pathToFfmpeg, [
                 '-re',
-                '-report', 'ffmpeg-%t.log',
+                '-report',
                 '-i', this.data.stream.event.recordingUrl,
                 '-map', '0:v',
                 '-map', '0:a',
