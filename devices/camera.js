01b86b2f3c235c6c5a08a69a3d556ad15c1aad02
tsightler <tsightler@gmail.com>
2022-10-18 13:50:08 -0400

    Update camera.js

--- devices/camera.js
+++ devices/camera.js
@@ -682,7 +682,8 @@ class Camera extends RingPolledDevice {
                 '-c:a:1', 'libopus',
                 '-pix_fmt', 'yuv420p',
                 '-c:v', 'h264_v4l2m2m',
-                '-preset', 'ultrafast',
+                '-preset', 'fast',
+                '-bf', '0',
                 '-f', 'rtsp',
                 '-rtsp_transport', 'tcp',
                 this.data.stream.event.rtspPublishUrl
