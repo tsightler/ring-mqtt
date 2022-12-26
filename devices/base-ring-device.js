707469a222d3d94a9ea682aa28262bd6ad48ff19
tsightler <tsightler@gmail.com>
2022-12-26 16:39:18 -0500

    Test

--- devices/base-ring-device.js
+++ devices/base-ring-device.js
@@ -178,10 +178,9 @@ class RingDevice {
                             utils.event.emit('mqtt_ipc_subscribe', streamDebugTopic)
                             utils.event.on(streamDebugTopic, (command, message) => {
                                 if (message) {
-                                    const thisMessage = message
-                                    const message2 = '\u001b[33mState indicates live stream has gone inactive\u001b[0m'
+                                    const message2 = JSON.parse(JSON.stringify(message))
                                     console.log(thisMessage.length, message2.length)
-                                    this.debug(thisMessage, 'rtsp')
+                                    this.debug(message, 'rtsp')
                                     this.debug(message2, 'rtsp')
                                 } else {
                                     this.debug(`Received invalid or null value to debug topic ${command}`)
--- scripts/start-stream.sh
+++ scripts/start-stream.sh
@@ -72,7 +72,8 @@ do
                 fi
                 ;;
             inactive)
-                mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" -m "${yellow}State indicates ${type} stream has gone inactive${reset}"
+                #mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" -m "${yellow}State indicates ${type} stream has gone inactive${reset}"
+                echo "${yellow}State indicates ${type} stream has gone inactive${reset}" | mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" =s
                 reason='inactive'
                 cleanup
                 ;;
