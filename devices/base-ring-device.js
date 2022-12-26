8671a9d8b58d7a640ee1ff8174be2e01bdeeb959
tsightler <tsightler@gmail.com>
2022-12-26 14:42:32 -0500

    Update base-ring-device.js

--- devices/base-ring-device.js
+++ devices/base-ring-device.js
@@ -178,8 +178,7 @@ class RingDevice {
                             utils.event.emit('mqtt_ipc_subscribe', streamDebugTopic)
                             utils.event.on(streamDebugTopic, (command, message) => {
                                 if (message) {
-                                    console.log(message)
-                                    this.debug(message, 'rtsp')
+                                    this.debug(message.toString(), 'rtsp')
                                 } else {
                                     this.debug(`Received invalid or null value to debug topic ${command}`)
                                 }
