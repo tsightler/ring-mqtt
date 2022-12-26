cec7ed42b0023acc4e2fe1849ccbaf5a91346378
tsightler <tsightler@gmail.com>
2022-12-26 16:04:51 -0500

    Update base-ring-device.js

--- devices/base-ring-device.js
+++ devices/base-ring-device.js
@@ -178,7 +178,7 @@ class RingDevice {
                             utils.event.emit('mqtt_ipc_subscribe', streamDebugTopic)
                             utils.event.on(streamDebugTopic, (command, message) => {
                                 if (message) {
-                                    this.debug(`${Buffer.from('message', 'utf-8').toString()}`, 'rtsp')
+                                    this.debug(`${Buffer.from(message, 'utf-8').toString()}`, 'rtsp')
                                     this.debug('\u001b[33mState indicates live stream has gone inactive\u001b[0m', 'rtsp')
                                 } else {
                                     this.debug(`Received invalid or null value to debug topic ${command}`)
