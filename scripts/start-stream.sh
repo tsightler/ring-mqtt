0e5a29d65fa27905e37dc82ede3d8a6e49fd0953
tsightler <tsightler@gmail.com>
2022-12-26 16:49:42 -0500

    Update start-stream.sh

--- scripts/start-stream.sh
+++ scripts/start-stream.sh
@@ -73,7 +73,7 @@ do
                 ;;
             inactive)
                 #mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" -m "${yellow}State indicates ${type} stream has gone inactive${reset}"
-                echo "${yellow}State indicates ${type} stream has gone inactive${reset}" | mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" =s
+                echo "${yellow}State indicates ${type} stream has gone inactive${reset}" | mosquitto_pub -i "${client_id}_pub" -L "mqtt://127.0.0.1:51883/${debug_topic}" -s
                 reason='inactive'
                 cleanup
                 ;;
