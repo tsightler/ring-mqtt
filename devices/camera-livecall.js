9be69c06c792b1bc0b0fed92cfb1b198df63a2dd
tsightler <tsightler@gmail.com>
2023-01-12 22:12:26 -0500

    Update camera-livecall.js

--- devices/camera-livecall.js
+++ devices/camera-livecall.js
@@ -46,7 +46,7 @@ parentPort.on("message", async(data) => {
                     streamData.rtspPublishUrl
                 ]
             })
-            console.log(liveCall)
+            console.log(liveCall.pc)
             parentPort.postMessage({ state: 'active' })
             liveCall.onCallEnded.subscribe(() => {
                 debug(chalk.green(`[${deviceName}] `)+'Live stream for camera has ended')
