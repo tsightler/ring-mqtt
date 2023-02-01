3737bc362e7130c1ebdb3db114014e8f5c2d0dbe
tsightler <tsightler@gmail.com>
2023-01-31 21:07:45 -0500

    Tweak AAC audio

--- devices/camera-livestream.js
+++ devices/camera-livestream.js
@@ -46,7 +46,7 @@ parentPort.on("message", async(data) => {
                     '-map', '0:v',
                     '-map', '0:a',
                     '-map', '0:a',
-                    '-c:a:0', 'aac',
+                    '-c:a:0', 'libfdk_aac',
                     '-c:a:1', 'copy',
                 ],
                 video: [
