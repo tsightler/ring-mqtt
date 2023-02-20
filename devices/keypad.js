164cd6c87d7e9aa6165256066009b2344d4b4baf
tsightler <tsightler@gmail.com>
2023-02-20 16:14:48 -0500

    Implement keypad motion sensor

--- devices/keypad.js
+++ devices/keypad.js
@@ -5,12 +5,45 @@ export default class Keypad extends RingSocketDevice {
         super(deviceInfo, 'alarm')
         this.deviceData.mdl = 'Security Keypad'
 
-        this.entity.volume = {
-            component: 'number',
-            min: 0,
-            max: 100,
-            icon: 'hass:volume-high'
+        this.data = {
+            motion: {
+                state: 'OFF',
+                publishedState: null,
+                timeout: false
+            }
         }
+
+        this.entity = {
+            ...this.entity,
+            volume: {
+                component: 'number',
+                min: 0,
+                max: 100,
+                icon: 'hass:volume-high'
+            },
+            motion: {
+                component: 'binary_sensor',
+                device_class: 'motion',
+                attributes: true
+            }
+        }
+
+        // Ugly, but this listens raw data updates for all devices to
+        // find proximity detection events for this keypad.
+        this.device.location.onDataUpdate.subscribe((message) => {
+            if (message.datatype === 'DeviceInfoDocType' && 
+                Boolean(message.body) && 
+                Array.isArray(message.body) &&
+                message.body[0]?.general?.v2?.zid === this.deviceId &&
+                message.body[0]?.impulse?.v1 &&
+                Boolean(message.body[0].impulse.v1) &&
+                Array.isArray(message.body[0].impulse.v1)
+            ) {
+                if (message.body[0].impulse.v1[0].impulseType === 'keypad.motion') {
+                    this.processMotion()
+                }
+            }
+        })
     }
 
     publishState(data) {
@@ -22,7 +55,29 @@ export default class Keypad extends RingSocketDevice {
 
         const currentVolume = (this.device.data.volume && !isNaN(this.device.data.volume) ? Math.round(100 * this.device.data.volume) : 0)
         this.mqttPublish(this.entity.volume.state_topic, currentVolume.toString())
+        this.publishMotionState(isPublish)
         this.publishAttributes()
+    }
+
+    publishMotionState(isPublish) {
+        if (this.data.motion.state !== this.data.motion.publishedState || isPublish) {
+            this.mqttPublish(this.entity.motion.state_topic, this.data.motion.state)
+            this.data.motion.publishedState = this.data.motion.state
+        }
+    }
+    
+    processMotion() {
+        if (this.data.motion.timeout) {
+            clearTimeout(this.motion.ding.timeout)
+            this.data.motion.timeout = false
+        }
+        this.data.motion.state = 'ON'
+        this.publishMotionState()
+        this.data.motion.timeout = setTimeout(() => {
+            this.data.motion.state = 'OFF'
+            this.publishMotionState()
+            this.data.motion.timeout = false
+        }, 20000)
     }
 
     // Process messages from MQTT command topic
