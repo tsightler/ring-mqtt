1454719e98cbf3e94faa8f9f211b5632a5b0c357
tsightler <tsightler@gmail.com>
2023-07-02 23:16:38 -0400

    mode ? mode : this.device.data.mode

--- devices/security-panel.js
+++ devices/security-panel.js
@@ -88,8 +88,7 @@ export default class SecurityPanel extends RingSocketDevice {
         }
 
         // If mode was passed to the function use it, oterwise use currently active device mode
-        mode = mode ? mode : this.device.data.mode
-        switch (mode) {
+        switch (mode ? mode : this.device.data.mode) {
             case 'none':
                 return 'disarmed'
             case 'some':
