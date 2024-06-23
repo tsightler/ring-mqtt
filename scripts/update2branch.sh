0dcaec99c2defaec927935b8a4ea46953a318f33
Tom Sightler <tom.sightler@veeam.com>
2024-06-22 21:02:42 -0400

    Test node v20

--- scripts/update2branch.sh
+++ scripts/update2branch.sh
@@ -25,18 +25,35 @@ else
     GO2RTC_VERSION="v1.9.2"
     case "${APK_ARCH}" in
         x86_64)
-            GO2RTC_ARCH="amd64";;
+            GO2RTC_ARCH="amd64"
+            ;;
         aarch64)
-            GO2RTC_ARCH="arm64";;
+            GO2RTC_ARCH="arm64"
+            ;;
         armv7|armhf)
-            GO2RTC_ARCH="arm";;
+            GO2RTC_ARCH="arm"
+            ;;
         *)
             echo >&2 "ERROR: Unsupported architecture '$APK_ARCH'"
-            exit 1;;
+            exit 1
+            ;;
     esac
     rm -f /usr/local/bin/go2rtc
     curl -L -s -o /usr/local/bin/go2rtc "https://github.com/AlexxIT/go2rtc/releases/download/${GO2RTC_VERSION}/go2rtc_linux_${GO2RTC_ARCH}"
     chmod +x /usr/local/bin/go2rtc
+
+    case "${APK_ARCH}" in
+        x86_64)
+            apk del npm nodejs
+            apk add libstdc++
+            cd /opt
+            wget https://unofficial-builds.nodejs.org/download/release/v20.14.0/node-v20.14.0-linux-x64-musl.tar.gz
+            mkdir nodejs
+            tar -zxvf *.tar.gz --directory /opt/nodejs --strip-components=1
+            ln -s /opt/nodejs/bin/node /usr/local/bin/node
+            ln -s /opt/nodejs/bin/npm /usr/local/bin/npm
+            ;;
+    esac
 
     cp -f "/app/ring-mqtt-${BRANCH}/init/s6/services.d/ring-mqtt/run" /etc/services.d/ring-mqtt/run
     chmod +x /etc/services.d/ring-mqtt/run
